7eff3d54e882c8cea541d44b0c1253333b59f30c
Aishwarya Roy <royaishwarya11@gmail.com>
2024-06-02 12:10:08 -0700

    Time: 3 ms (90.56%), Space: 43.5 MB (57.54%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,28 +1,19 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] finalArr = new int[nums1.length];
-        //nums1 = {2,4} nums2 ={1,3,4,2}
-        for(int i = 0;i<nums1.length;i++){  //0
-            for(int j = 0;j< nums2.length;j++){ //0
-                if(nums1[i] == nums2[j]){ // 2&1, 2&3 , 2&4, 2&2
-                    finalArr[i] = findNextGreater(nums2, j);
-                }
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        for(int num : nums2){
+            while(!stack.isEmpty() && stack.peek() < num){
+                map.put(stack.pop() , num);
             }
-
+            stack.push(num);
         }
-        return finalArr;
+        for(int i = 0;i<nums1.length;i++){
+            nums1[i] = map.getOrDefault(nums1[i], -1);
+        }
+       
+        return nums1;
         
     }
-    private static int findNextGreater(int []nums2, int j){
-        int right = j+1; //4+1 = 5
-
-        while(right <nums2.length){
-            if(nums2[j] < nums2[right]){
-                return nums2[right];
-            }
-            right++;
-        }
-        return -1;
-
-    }
+   
 }
