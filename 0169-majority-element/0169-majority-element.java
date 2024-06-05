08fb6e4579e81f62054fec22446fd0a61e36b544
Aishwarya Roy <royaishwarya11@gmail.com>
2024-06-05 10:49:29 -0700

    Time: 381 ms (6.39%), Space: 50.8 MB (69.02%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,27 +1,26 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int candidate = nums[0];
-        int count = 1;
-        for(int i = 1;i<nums.length;i++){
-            if(candidate == nums[i])
-            count++;
-            else{
-                count--;
-                if(count == 0){
-                    candidate = nums[i];
-                    count = 1;
-                }
+        Map<Integer, Integer> map = new HashMap<>();
+        
+
+        for(int i = 0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                System.out.println(map);
+                map.put(nums[i] , map.get(nums[i]) + 1);
+            }else if(!map.containsKey(nums[i])){
+                  map.put(nums[i], 1);
+
             }
+          
+
         }
-        int occurences = 0;
-        for(int num :nums){
-            if(num == candidate){
-                occurences++;
-            }
-        }
-        if(occurences > nums.length/2)
-        return candidate;
-        else
-        return -1;   
+        System.out.println(map);
+        List <Integer> list = new ArrayList<>(map.keySet());
+        list.sort((a,b) -> map.get(a) -  map.get(b));
+        System.out.println(list);
+        int lastElementIndex = list.size()-1;
+
+        return list.get(lastElementIndex);
+
     }
 }
