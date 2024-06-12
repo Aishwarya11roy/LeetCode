2897cdfa69e2f71ecee3e48d3088f83e2514ffe0
Aishwarya Roy <royaishwarya11@gmail.com>
2024-06-11 22:50:30 -0700

    Time: 19 ms (11.77%), Space: 48.2 MB (67.36%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,25 +1,22 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i = 0;i<nums.length;i++){
-            if(!(map.containsKey(nums[i]))){
-                map.put(nums[i],1);
-            }
-            else{
-                map.put(nums[i],map.get(nums[i])+1);   
+
+        HashMap<Integer, Integer>map = new HashMap<>();
+        for(int num : nums){
+            if(!map.containsKey(num)){
+                map.put(num, 1);
+            }else{
+                map.put(num, map.get(num) + 1);
             }
         }
-        List<Map.Entry <Integer,Integer>> list = new ArrayList<>(map.entrySet());
-        list.sort((o1 , o2)->{
-            int cmp = o2.getValue().compareTo(o1.getValue());
-           return (cmp == 0) ? o1.getKey().compareTo(o2.getKey()) : cmp;
-        });
-        int result[] = new int[k];
-        for (int i = 0; i < k && i < list.size(); i++) {
-            result[i] = list.get(i).getKey();
+        List <Integer> list = new ArrayList<>(map.keySet());
+        System.out.println(list);
+        list.sort((a,b) -> map.get(b) -  map.get(a));
+        int [] result = new int[k];
+        for(int i = 0;i<k;i++){
+            result[i] = list.get(i);
         }
         return result;
 
-        
     }
 }
