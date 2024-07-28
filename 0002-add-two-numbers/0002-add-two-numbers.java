bc334ed5dae5551c1e48e8d8a6420bd3dd1c3f82
Aishwarya Roy <royaishwarya11@gmail.com>
2024-07-28 09:28:35 +0530

    Time: 1 ms (100%), Space: 45 MB (7.65%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,34 +10,32 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummyHead = new ListNode(0); // Create a dummy head node
-        ListNode result = dummyHead; // Initialize a result node pointing to the dummy head
-        int carry = 0; // Initialize carry to 0
 
-        while (l1 != null || l2 != null) {
-            int sum = carry; // Start with the carry value
+        ListNode dummy = new ListNode();
+        ListNode temp = dummy;
+        int carry = 0;
+        
 
-            if (l1 != null) {
-                sum += l1.val; // Add the value from l1
-                l1 = l1.next; // Move to the next node in l1
+        while(l1 != null || l2!= null || carry == 1){
+            int sum = 0;
+            if(l1 != null){
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                sum+= l2.val;
+                l2 = l2.next;
             }
 
-            if (l2 != null) {
-                sum += l2.val; // Add the value from l2
-                l2 = l2.next; // Move to the next node in l2
-            }
+            sum+= carry; // total sum i have got
+            carry = sum/10;
+            ListNode newNode = new ListNode(sum % 10);
+            temp.next = newNode; // add new node to the next of dummy node
+            temp = temp.next; // move to the diiferent node
 
-            carry = sum / 10; // Update the carry for the next iteration
-            sum = sum % 10; // Calculate the value to be stored in the result node
 
-            result.next = new ListNode(sum); // Create a new node with the calculated value
-            result = result.next; // Move the result pointer to the next node
         }
-
-        if (carry == 1) {
-            result.next = new ListNode(1); // If there is a remaining carry, add an additional node with a value of 1
-        }
-
-        return dummyHead.next; // Return the next node after the dummy head (actual result)
+        return dummy.next;
+        
     }
 }
