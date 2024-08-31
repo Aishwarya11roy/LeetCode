fe49311e0be600320b669629c48afcd4387e66b1
Aishwarya Roy <royaishwarya11@gmail.com>
2024-08-31 23:45:56 +0530

    Time: 1 ms (85.25%), Space: 41.5 MB (87.18%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,32 +1,17 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
 
-        char[] word1ChToArr = word1.toCharArray();
-        char[] word2ChToArr = word2.toCharArray();
-        int totalLength = word1ChToArr.length + word2ChToArr.length;
-        char[] mergedArr = new char[totalLength];
-
-        int index1 = 0; // index for word1
-        int index2 = 0; // index for word2
-        int i = 0; // index for mergedArr
-
-        // Merge characters from both words alternately
-        while (index1 < word1.length() && index2 < word2.length()) {
-            mergedArr[i++] = word1ChToArr[index1++];
-            mergedArr[i++] = word2ChToArr[index2++];
+        StringBuilder merged = new StringBuilder();
+        int index = 0;
+        while(index < word1.length() || index < word2.length()){
+            if(index < word1.length()){
+                merged.append(word1.charAt(index));
+            }
+            if(index < word2.length()){
+                merged.append(word2.charAt(index));
+            }
+            index++;
         }
-
-        // If any characters are left in word1
-        while (index1 < word1.length()) {
-            mergedArr[i++] = word1ChToArr[index1++];
-        }
-
-        // If any characters are left in word2
-        while (index2 < word2.length()) {
-            mergedArr[i++] = word2ChToArr[index2++];
-        }
-
-        // Convert the merged array to a string
-        return new String(mergedArr, 0, i);
+        return merged.toString();
     }
 }
