bea529c0a23bea394619d0598245293fb7620462
Aishwarya Roy <royaishwarya11@gmail.com>
2024-09-09 17:24:58 +0530

    Time: 14 ms (29.31%), Space: 44.4 MB (48.36%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,18 +1,27 @@
+import java.util.HashMap;
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
-            return false;
+        if (s.length() != t.length()) {
+            return false; // If lengths are different, they can't be anagrams
         }
-        char[] sChar = s.toCharArray();
-        char[] tChar = t.toCharArray();
-        Arrays.sort(sChar);
-        Arrays.sort(tChar);
-        for(int i = 0;i<s.length();i++){
-            if(sChar[i] != tChar[i]){
+        HashMap<Character, Integer> charCountMap = new HashMap<>();
+
+        // Count characters in the first string
+        for (char c : s.toCharArray()) {
+            charCountMap.put(c, charCountMap.getOrDefault(c, 0) + 1);
+        }
+
+        // Decrease counts based on the second string
+        for (char c : t.toCharArray()) {
+            // If character not found or count reaches zero, they are not anagrams
+            if (!charCountMap.containsKey(c) || charCountMap.get(c) == 0) {
                 return false;
             }
+            charCountMap.put(c, charCountMap.get(c) - 1);
         }
-        return true;
-        
+
+        // No need for additional checks since counts won't be negative
+        return true; // All counts balanced correctly, so the strings are anagrams
     }
 }
