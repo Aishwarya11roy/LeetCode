d2b7cae583f74f557b6c2f4a7de5b1eed681456a
Aishwarya Roy <royaishwarya11@gmail.com>
2024-10-06 23:23:24 +0530

    Time: 3 ms (92.94%), Space: 43.7 MB (45.84%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,19 +1,24 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> map = new HashMap<>();
+
         Stack<Integer> stack = new Stack<>();
-        for(int num : nums2){
-            while(!stack.isEmpty() && stack.peek() < num){
-                map.put(stack.pop() , num);
+        Map<Integer , Integer> map = new HashMap<>();
+        /**
+        1st we need to iterate over the nums2 and for each element in the nums2 array we need to find the next greater element and need to put this in the map
+         */
+        for(int n : nums2){
+            while(!stack.isEmpty() && n>stack.peek()){
+                map.put(stack.pop(), n);
+
             }
-            stack.push(num);
+            stack.push(n);
         }
+        // iterate over the nums1 and check in the map if it exist just get the map's value for the next greater element otherwise just return -1
         for(int i = 0;i<nums1.length;i++){
-            nums1[i] = map.getOrDefault(nums1[i], -1);
+            nums1[i] = map.getOrDefault(nums1[i],-1);
         }
-       
-        return nums1;
-        
+         return nums1;
+
     }
-   
+         
 }
