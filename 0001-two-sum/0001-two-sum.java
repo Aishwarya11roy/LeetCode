b01b5853743ec61442fafe71ed8baa7111660568
Aishwarya Roy <royaishwarya11@gmail.com>
2024-10-28 21:39:50 +0530

    Time: 5 ms (57.03%), Space: 44.7 MB (62.76%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,25 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer,Integer> map = new HashMap<>();
-        int []  arr = new int[2];
-        for(int i = 0;i<nums.length;i++){
+        Map<Integer, Integer> map = new HashMap<>();
+        int [] arr = new int[2];
+        for(int i = 0; i< nums.length;i++){
             if(map.containsKey(target - nums[i])){
-                arr[0] = i;
-                arr[1] = map.get(target-nums[i]);
+                arr[0] = map.get(target-nums[i]);
+                arr[1] = i;
+            }else{
+                map.put(nums[i] , i);
             }
-            map.put(nums[i],i);
         }
-        return arr;   
+        return arr;
+        
     }
 }
+/**
+Space Complexity
+HashMap Storage (O(n)):
+
+In the worst case, if no two numbers add up to the target, we may end up storing each element of nums in the map. This would require O(n) space.
+Result Array:
+
+The result array (arr) has a fixed size of 2, so it requires O(1) space.
+ */
