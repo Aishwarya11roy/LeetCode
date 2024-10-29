c393d8dbfee699c6eda73a26f07d061029b537eb
Aishwarya Roy <royaishwarya11@gmail.com>
2024-10-29 13:20:25 +0530

    Time: 0 ms (100%), Space: 42.6 MB (49.03%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -15,13 +15,31 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
+
         if(root == null){
             return 0;
         }
-        else{
-            int leftHeight = maxDepth(root.left);
-            int rightHieght = maxDepth(root.right);
-            return Math.max(leftHeight , rightHieght) + 1;
-        }
+        int leftHeight = maxDepth(root.left);
+        int rightHeight = maxDepth(root.right);
+        return Math.max(leftHeight , rightHeight)+1;
+        
     }
 }
+/**
+time complexity : o(n)
+n : being number of nodes in the tree
+space complexitu : o(h)
+height of the tree
+
+Explanation: The space complexity comes from the recursion stack used during the depth-first traversal. The maximum depth of the recursion stack will be equal to the height of the tree:
+For a balanced tree (e.g., a perfect binary tree), the height 
+ℎ
+h is approximately 
+logn, so the space complexity in this case is 
+O(logn).
+
+For a skewed tree (e.g., a tree where each node has only one child), the height 
+h could be equal to 
+n, resulting in a space complexity of 
+O(n).
+ */
